56a9dbccf5fb74f23cf2583e3f8ed6919b85e41e
Cetin-Kurt <cetinkurt@gmail.com>
2025-06-30 20:45:50 +0200

    db

--- app/app/test.py
+++ app/app/test.py
@@ -11,7 +11,7 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         """Test adding numbers together"""
         res = calc.add(5, 6)
-        self.assertEqual(res, 11)
+        self.assertEqual(res, 21)
 
     def test_add_numbers_negative(self):
         """Test adding negative numbers"""
